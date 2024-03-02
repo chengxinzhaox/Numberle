df0ba69739bc14b66ac565780b0c6967cbf33145
Chengxin_zhao <zchenxing2002@gmail.com>
2024-03-02 12:43:45 +0800

    v3.01
    - refined some structures

--- src/NumberleCLI.java
+++ src/NumberleCLI.java
@@ -1,7 +1,6 @@
 import models.CalculationException;
 import models.GameModel;
 import models.IGameModel;
-import views.CLIColors;
 import views.Messages;
 
 import java.util.Scanner;
@@ -175,6 +174,24 @@ public class NumberleCLI {
             } else {
                 printGray(Character.toString(listChar));
             }
+        }
+    }
+
+    private enum CLIColors {
+        ORANGE("\033[33m"),
+        GREEN("\033[32m"),
+        GRAY("\033[37m"),
+        RED("\033[31m"),
+        RESET("\033[0m");
+
+        private final String code;
+
+        CLIColors(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
         }
     }
 
