4b394c265ac9e506cef7d95001894d67692943b6
Chengxin Zhao <99518086+ChesterZhao@users.noreply.github.com>
2024-04-16 14:42:22 +0800

    v3.5.1
    multithreading

--- src/GUIApp.java
+++ src/GUIApp.java
@@ -5,6 +5,12 @@ import views.View;
 
 public class GUIApp {
     public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(
+                GUIApp::createAndShowGUI
+        );
+    }
+
+    public static void createAndShowGUI() {
         IModel model = new Model();
         Controller controller = new Controller(model);
         new View(model, controller);
