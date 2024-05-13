76dd6b7f5809dfe03df1856cec330217ac4af4ea
Chengxin Zhao <99518086+ChesterZhao@users.noreply.github.com>
2024-05-13 14:11:46 +0800

    v3.5.4
    - add print categories function in CLI model

--- src/CLIApp.java
+++ src/CLIApp.java
@@ -4,12 +4,20 @@ import models.Model;
 import models.IModel;
 import views.Messages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class CLIApp {
 
     private final IModel model;
     private final Scanner scanner;
+
+    private final List<String> orangeList;
+    private final List<String> greenList;
+    private final List<String> grayList;
+    private final List<String> withoutUse;
 
     private CLIApp() {
         this.model = new Model();
@@ -18,6 +26,11 @@ public class CLIApp {
         model.setShowErrorFlag(getFlag(Messages.SHOW_ERROR_MESSAGE));
         model.setShowEquationFlag(getFlag(Messages.SHOW_EQUATION_MESSAGE));
         model.setRandomEquationFlag(getFlag(Messages.RANDOM_EQUATION_MESSAGE));
+
+        orangeList = new ArrayList<>();
+        greenList = new ArrayList<>();
+        grayList = new ArrayList<>();
+        withoutUse = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/"));
     }
 
     /**
@@ -38,6 +51,7 @@ public class CLIApp {
             try {
                 if (model.guessVerification(guess)) {
                     model.updateUserLog(guess);
+                    printCategories(guess);
                     printUserLog(model.getUserLog(), model.getEquation());
                 }
             } catch (CalculationException e) {
@@ -153,6 +167,93 @@ public class CLIApp {
         }
         System.out.println(Messages.CUT_OFF);
     }
+
+    /**
+     * PrintCategories to CLI output
+     *
+     * @param guess the user's guess
+     */
+    private void printCategories(String guess) {
+        System.out.println(Messages.CUT_OFF);
+        updateCategories(guess);
+        removeConflicts();
+        printResults();
+        System.out.println(Messages.CUT_OFF);
+    }
+
+    /**
+     * Update the categories
+     *
+     * @param guess the user's guess
+     */
+    private void updateCategories(String guess) {
+        for (int i = 0; i < guess.length(); i++) {
+            char guessedChar = guess.charAt(i);
+            CharType charType = model.checkCharType(guessedChar, i);
+            addCharToCategory(String.valueOf(guessedChar), charType);
+        }
+    }
+
+    /**
+     * Add the character to the category
+     *
+     * @param character the character to add
+     * @param charType  the type of the character
+     */
+    private void addCharToCategory(String character, CharType charType) {
+        switch (charType) {
+            case GREEN:
+                if (addToCategoryIfAbsent(greenList, character)) {
+                    withoutUse.remove(character);
+                }
+                break;
+            case ORANGE:
+                if (!greenList.contains(character) && addToCategoryIfAbsent(orangeList, character)) {
+                    withoutUse.remove(character);
+                }
+                break;
+            case GRAY:
+                if (!greenList.contains(character) && !orangeList.contains(character) && addToCategoryIfAbsent(grayList, character)) {
+                    withoutUse.remove(character);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Add to category if absent
+     *
+     * @param category  the category to add to
+     * @param character the character to add
+     * @return true if the character is added, false otherwise
+     */
+    private boolean addToCategoryIfAbsent(List<String> category, String character) {
+        if (!category.contains(character)) {
+            category.add(character);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Remove duplicates from the categories
+     */
+    private void removeConflicts() {
+        orangeList.removeAll(greenList);
+        grayList.removeAll(greenList);
+        grayList.removeAll(orangeList);
+    }
+
+    /**
+     * Print the results
+     */
+    private void printResults() {
+        printGreen("Green: " + greenList + "\n");
+        printOrange("Orange: " + orangeList + "\n");
+        printGray("Gray: " + grayList + "\n");
+        System.out.println("Without Use: " + withoutUse);
+    }
+
 
     /**
      * printSingleList
